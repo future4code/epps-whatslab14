c6dc52cad254799734dec89c7084243387719059
Yuri Pinheiro <yuripinheirot@gmail.com>
2020-12-18 13:51:25 -0400

    Criado botao e iniciando deletar

--- src/components/Mensagens/Mensagens.js
+++ src/components/Mensagens/Mensagens.js
@@ -3,9 +3,17 @@ import React from 'react'
 
 const DivMsg = styled.div`
     display: flex;
+    flex-direction: ${props => props.usuario === 'eu' ? 'row-reverse' : 'rowss'};
+    justify-content: space-between; 
     background-color: lightgrey;
     margin: 2px 0px;
 `
+
+const BotaoDeletar = styled.button`
+background-color: #ff4646;
+`
+
+
 
 export default class Footer extends React.Component {
 
@@ -15,9 +23,12 @@ export default class Footer extends React.Component {
 
     render() {
         return (
-           <DivMsg>
-               <p>{this.props.usuario}: {this.props.mensagem}</p>
-           </DivMsg>
+            <div>
+                <DivMsg>
+                    <p>{this.props.usuario}: {this.props.mensagem}</p>
+                <BotaoDeletar onClick={this.props.DeletarMensagem}>X</BotaoDeletar>
+                </DivMsg>
+            </div>
         )
     }
 
